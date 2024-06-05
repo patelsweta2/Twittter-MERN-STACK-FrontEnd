abcd36e184dd593cfd79a8173b83bf231bf1500a
patelsweta2 <sweta0442@gmail.com>
2024-06-05 12:39:27 +0530

    setup main.jsx with lazyloading , store and route

--- frontend/src/components/loading/Loading.jsx
+++ frontend/src/components/loading/Loading.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <div className="w-full grid place-items-center h-screen">
+      <span className="loading"></span>
+    </div>
+  );
 };
 
 export default Loading;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App.jsx";
+import Loading from "./components/loading/Loading.jsx";
+import { store } from "./store.js";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Suspense fallback={<Loading />}>
+    <React.StrictMode>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/*" element={<App />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </React.StrictMode>
+  </Suspense>
+);
