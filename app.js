5b3f88df0204e5dc91a92f7e8b8c16f057a99988
patelsweta2 <sweta0442@gmail.com>
2024-06-05 12:10:02 +0530

    Implement Cors

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const constants = require("./constants");
+const cors = require("cors");
 
 const app = express();
+
+const corsOptions = {
+  origin: constants.ALLOWED_URLS,
+  credentials: true,
+  method: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  exposedHeaders: ["Access-Control-Allow-Private-Network"],
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors());
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(cookieParser());
 
 module.exports = app;
