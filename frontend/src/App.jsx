264fae74b9e4ceb8227cbdbaa5e2db86c58b822c
patelsweta2 <sweta0442@gmail.com>
2024-06-05 15:14:20 +0530

    add some pages in app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import PresistLogin from "./components/PresistLogin.jsx";
+import AuthLayout from "./layouts/AuthLayout.jsx";
+const RegisterPage = React.lazy(() => import("./pages/RegisterPage.jsx"));
+const LoginPage = React.lazy(() => import("./pages/LoginPage.jsx"));
+const ResetPasswordPage = React.lazy(() =>
+  import("./pages/ResetPasswordPage.jsx")
+);
 
 const App = () => {
   return (
-    <>
-      <div>App</div>
-    </>
+    <Routes>
+      <Route path="*" element={<ErrorPage />} />
+      <Route element={<AuthLayout />}>
+        <Route path="" index element={<LoginPage />} />
+        <Route path="" index element={<RegisterPage />} />
+        <Route path="" index element={<ResetPasswordPage />} />
+      </Route>
+    </Routes>
   );
 };
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,19 +4,19 @@ import { Provider } from "react-redux";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import Loading from "./components/loading/Loading.jsx";
-import { store } from "./store.js";
+// import { store } from "./store.js";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Suspense fallback={<Loading />}>
     <React.StrictMode>
-      <Provider store={store}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/*" element={<App />} />
-          </Routes>
-        </BrowserRouter>
-      </Provider>
+      {/* <Provider store={store}> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+      {/* </Provider> */}
     </React.StrictMode>
   </Suspense>
 );
